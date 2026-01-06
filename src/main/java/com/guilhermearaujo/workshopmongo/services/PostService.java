6dd50018c3ac0e1818454fd3e73d9e0ad63afefb
Guilherme Araújo <guilhermearaujofsa2007@gmail.com>
2026-01-06 09:01:55 -0300

    Post findById

---
+++ src/main/java/com/guilhermearaujo/workshopmongo/services/PostService.java
@@ -0,0 +1,27 @@
+package com.guilhermearaujo.workshopmongo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.guilhermearaujo.workshopmongo.domain.Post;
+import com.guilhermearaujo.workshopmongo.repositories.PostRepository;
+import com.guilhermearaujo.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository postRepository;
+	
+	public List<Post> findAll() {
+		return postRepository.findAll();
+	}
+	
+	public Post findById(String id) {
+		Optional<Post> post = postRepository.findById(id);
+		return post.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+}
